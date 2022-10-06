cc37544311682152e9bf7d3f3d428fb18d3b3775
Dell PC <monu40128@gmail.com>
2022-10-06 14:55:52 +0530

    testing

--- make _pattern.py
+++ make _pattern.py
@@ -1,26 +1,26 @@
-n=int(input('Enter a odd number'))
-if(n%2!=0 and n>=5):
+n = int(input('Enter a odd number for pattern'))
+if (n % 2 != 0 and n >= 5):
     for i in range(n):
-        if(i==0):
-            print('* '*((n+1)//2),end='')
-            print('  '*(((n+1)//2)-2),end='')
-            print('*',end='')
-        elif(i>0 and i<(n//2)):
-            print('  '*(n//2),end='')
-            print('* ',end='')
-            print('  '*(((n+1)//2)-2),end='')
-            print('*',end='')
-        elif(i==(n//2)):
-            print('* '*n,end='')
-        elif(i>(n//2) and i<(n-1)):
-            print('* ',end='')
-            print('  '*(((n+1)//2)-2),end='')
-            print('*',end='')
-        elif(i==(n-1)):
-            print('* ',end='')
-            print('  '*(((n+1)//2)-2),end='')
-            print('* '*((n+1)//2),end='')
+        if (i == 0):
+            print('* ' * ((n + 1) // 2), end='')
+            print('  ' * (((n + 1) // 2) - 2), end='')
+            print('*', end='')
+        elif (i > 0 and i < (n // 2)):
+            print('  ' * (n // 2), end='')
+            print('* ', end='')
+            print('  ' * (((n + 1) // 2) - 2), end='')
+            print('*', end='')
+        elif (i == (n // 2)):
+            print('* ' * n, end='')
+
+        elif (i > (n // 2) and i < (n - 1)):
+            print('* ', end='')
+            print('  ' * (((n + 1) // 2) - 2), end='')
+            print('*', end='')
+        elif (i == (n - 1)):
+            print('* ', end='')
+            print('  ' * (((n + 1) // 2) - 2), end='')
+            print('* ' * ((n + 1) // 2), end='')
         print()
 else:
     print('Only odd number and greater then 4 please!')
-    
